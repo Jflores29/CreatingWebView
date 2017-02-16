e92a76b007d6c8ce8f9c22ea422a7619cf5e9287
Javier <jflores71814@yahoo.com>
2017-02-16 13:18:36 -0500

    first commit

---
+++ app/src/main/java/com/javier/creatingwebview/MainActivity.java
@@ -0,0 +1,50 @@
+package com.javier.creatingwebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private EditText editText;
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.main_et);
+        button = (Button) findViewById(R.id.main_bttn);
+
+        webView = (WebView) findViewById(R.id.main_webvw);
+        webView.setWebViewClient(new MyBrowser());
+
+
+
+    }
+
+    public void goToWeb(View view) {
+        String url = editText.getText().toString();
+
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl("https://"+url);
+    }
+
+    private class MyBrowser extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+    }
+}
